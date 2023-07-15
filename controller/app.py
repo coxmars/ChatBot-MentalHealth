24915d59d2ad3456a287862d50b948a4b053f226
Marco Araya Jimenez <maraya80112@ufide.ac.cr>
2023-07-15 02:17:29 -0600

    Estructura inicial del proyecto con algunas configuraciones

---
+++ controller/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+# Here we create a Flask application object and pass __name__ to it.
+app = Flask(__name__)
+
+# Here we define the route and the function that is called when it
+@app.get("/")
+def index_get():
+    return render_template('base.html')
+
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    # TODO: Check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,96 @@
+class ChatBox {
+
+    // Constructor
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button'),
+        }
+        this.state = false;
+        this.messages = [];
+    }
+
+    // Initialize the chat box
+    display() {
+        const { openButton, chatBox, sendButton } = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox));
+
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox));
+
+        const node = chatBox.querySelector('input');
+
+        node.addEventListener('keyup', ({ key }) => {
+            if (key === 'Enter') {
+                this.onSendButton(chatBox)
+            }
+        });
+    }
+
+    // Toggle the chat box state
+    toggleState(chatBox) {
+        this.state = !this.state;
+        // Show or hides the box
+        if (this.state) {
+            chatBox.classList.add('chatbox--active');
+        }
+        else {
+            chatBox.classList.remove('chatbox--active');
+        }
+    }
+
+    // Send the message to the server
+    onSendButton(chatBox) {
+        var textField = chatBox.querySelector('input');
+        let text1 = textField.value;
+        if (text1 === "") {
+            return;
+        }
+
+        let message1 = { name: "User", message: text1 };
+        this.messages.push(message1);
+
+        // http://127.0.0.1:5000
+        fetch($SCRIPT_ROOT + '/predict', {
+            method: 'POST',
+            body: JSON.stringify({ message: text1 }),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(response => {
+            let message2 = { name: 'Jarvis', message: response.answer };
+            this.messages.push(message2);
+            this.updateChatText(chatBox);
+            textField.value = '';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatBox);
+            textField.value = '';
+        });
+    }
+
+    // Update the chat text
+    updateChatText(chatBox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(item => {
+            if (item.name == 'Jarvis') {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>';
+            }
+            else {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>';
+            }
+        });
+        const chatText = chatBox.querySelector('.chatbox__messages');
+        chatText.innerHTML = html;
+    }
+
+}
+
+// Initialize the chat box
+const chatbox = new ChatBox();
+chatbox.display();
