385f8eeb19bb19e587c8773513003bb1917158cc
Marco Araya Jimenez <maraya80112@ufide.ac.cr>
2023-07-15 02:48:21 -0600

    Errores corregidos en app.py para las rutas/carpetas

--- controller/app.py
+++ controller/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 
-from chat import get_response
+#from chat import get_response
 
 # Here we create a Flask application object and pass __name__ to it.
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../view', static_folder='../static')
 
 # Here we define the route and the function that is called when it
 @app.get("/")
 def index_get():
-    return render_template('base.html')
+    return render_template('index.html')
 
-
+"""
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
@@ -18,7 +18,7 @@ def predict():
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
-
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
